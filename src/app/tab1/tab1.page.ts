f3c25412f28bca9d1e59ce5eda897eb542970257
Víctor Romário <victor.romario.seriado@gmail.com>
2019-05-03 00:40:15 -0300

    Calculando o valor de pontos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -13,15 +13,18 @@ export class Tab1Page {
   public taxa:any;
   public dolar:any;
   public valorAproximado:any;
+  public pontos:any;
   public primeiroAno:Array<object> = [];
   public segundoAno:Array<object> = [];
 
   constructor(public http: Http) {
     this.dolar = 3;
     this.valorAproximado = 0;
+    this.pontos = 0;
     this.getDollar();
   }
 
+  //Busca e calcula o valor do dollar
   getDollar() {
     this.http.get('http://data.fixer.io/api/latest?access_key=5d0eb200bb9fb42872430c207728160f')
       .map(res => res.json()).subscribe(data => {
@@ -32,14 +35,18 @@ export class Tab1Page {
     });
   }
 
+  //método chamado pelo clique do botão simular
   getSimular() {
-    this.taxa = this.taxa;
     this.valorAproximado = (this.investimento/this.dolar).toFixed(2);
+    this.pontos = (this.valorAproximado / 50).toFixed(1);
     this.calculoMeses(this.investimento, this.taxa, this.dolar);
   }
 
+  //Cálculo dos rendimentos do primeiro e segundo ano
   calculoMeses(investimento:any, taxa:any, dolar:any) {
-
+    console.log(investimento);
+    console.log(taxa);
+    console.log(dolar);
   }
 
 }
