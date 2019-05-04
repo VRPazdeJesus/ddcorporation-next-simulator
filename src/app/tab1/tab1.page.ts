b7457fb497975c90d886125c69d4d1d256c4bb90
Víctor Romário <victor.romario.seriado@gmail.com>
2019-05-04 13:18:16 -0300

    Mudança no calculo do app

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-// import { Http } from '@angular/http';
-// import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-tab1',
@@ -9,74 +7,53 @@ import { Component } from '@angular/core';
 })
 
 export class Tab1Page {
-  public investimento:any;
-  public investimentoArredondado:any;
-  public taxa:any;
-  public dolar:any;
-  public valorEquivalenteDollar:any;
-  public pontos:any;
+  public dollar:number;
+  public valorAplicado:number;
+  public valorInvestido:number;
+  public taxa:number;
+  public totalRendimento:number;
+  public rendimentoMensal:number;
+  public rendimentoValorInvestido:number;
+  public valorInvestidoDollar:number;
+  public pontos:number;
   public primeiroAno:Array<number> = [];
-  public totalPrimeiroAno:any;
-  public totalPrimeiroAnoRendimentos:any;
-  public totalPrimeiroAnoMes:any;
   public segundoAno:Array<number> = [];
 
-  // constructor(public http: Http) {
-  //   this.dolar = 3;
-  //   this.valorEquivalenteDollar = 0;
-  //   this.pontos = 0;
-  //   this.investimentoArredondado = 0;
-  //   this.totalPrimeiroAno = 0;
-  //   this.totalPrimeiroAnoRendimentos = 0;
-  //   this.totalPrimeiroAnoMes = 0;
-  //   this.getDollar();
-  // }
-
   constructor() {
-    this.dolar = 3.30;
-    this.valorEquivalenteDollar = 0;
+    this.dollar = 3.30;
+    this.valorAplicado = 0;
+    this.valorInvestido = 0;
+    this.taxa = 0;
+    this.totalRendimento = 0;
+    this.rendimentoMensal = 0;
+    this.rendimentoValorInvestido = 0;
+    this.valorInvestidoDollar = 0;
     this.pontos = 0;
-    this.investimentoArredondado = 0;
-    this.totalPrimeiroAno = 0;
-    this.totalPrimeiroAnoRendimentos = 0;
-    this.totalPrimeiroAnoMes = 0;
   }
-
-  //Busca e calcula o valor do dollar
-  // getDollar() {
-  //   this.http.get('http://data.fixer.io/api/latest?access_key=5d0eb200bb9fb42872430c207728160f')
-  //     .map(res => res.json()).subscribe(data => {
-  //       let taxaDolar = data.rates['USD'];
-  //       let taxaReal = data.rates['BRL'];
-  //       let valorDolarReal = (taxaReal/taxaDolar).toFixed(2);
-  //       this.dolar = valorDolarReal;
-  //   });
-  // }
 
   //método chamado pelo clique do botão simular
   getSimular() {
-    this.marred(this.investimento,165);
-    this.valorEquivalenteDollar = (this.investimentoArredondado/this.dolar).toFixed(2);
-    this.pontos = ((this.valorEquivalenteDollar / 50)*8).toFixed(1);
-    this.calculoMeses(this.investimentoArredondado, this.taxa);
+    this.marred(this.valorAplicado,165);
+    this.valorInvestidoDollar = this.valorInvestido/this.dollar;
+    this.pontos = (this.valorInvestidoDollar / 50)*8;
+    this.calcularMeses(this.valorInvestidoDollar, this.taxa);
   }
 
-  marred(numero:any, multiplo:any) {
+  marred(valorAplicado:number, multiplo:number) {
     //50 dolares x 3,30 da 165 reais
-    let quantidade = numero / multiplo;
-    this.investimentoArredondado = Math.trunc(quantidade) * 165;
+    let quantidade = valorAplicado / multiplo;
+    this.valorInvestido = Math.trunc(quantidade) * 165;
   }
 
   //Cálculo dos rendimentos do primeiro e segundo ano
-  calculoMeses(investimento:any, taxa:any) {
+  calcularMeses(valorInvestido:number, taxa:number) {
     //investimentos do primeiro ano
     for(var i=0; i<12; i++) {
-      this.primeiroAno[i] = parseFloat(investimento*(taxa/100));
-      this.totalPrimeiroAno = this.totalPrimeiroAno + this.primeiroAno[i];
+      this.primeiroAno[i] = valorInvestido*(taxa/100);
+      this.totalRendimento = this.totalRendimento + this.primeiroAno[i];
     }
-    this.totalPrimeiroAnoRendimentos = (parseFloat(investimento) + parseFloat(this.totalPrimeiroAno)).toFixed(2);
-    this.totalPrimeiroAnoMes = parseFloat(this.primeiroAno[0]).toFixed(2);
-    this.totalPrimeiroAno = parseFloat(this.totalPrimeiroAno).toFixed(2);
+    this.rendimentoValorInvestido = valorInvestido + this.totalRendimento;
+    this.rendimentoMensal = this.primeiroAno[0];
   }
 
 }
