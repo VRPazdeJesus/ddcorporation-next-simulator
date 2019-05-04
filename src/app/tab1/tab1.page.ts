2a05062ef2b71035d66d923d43dc80578cfd1de7
Víctor Romário <victor.romario.seriado@gmail.com>
2019-05-04 11:20:01 -0300

    Calculo de married feito e geração do app debug

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+// import { Http } from '@angular/http';
+// import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-tab1',
@@ -21,34 +21,50 @@ export class Tab1Page {
   public totalPrimeiroAnoMes:any;
   public segundoAno:Array<number> = [];
 
-  constructor(public http: Http) {
-    this.dolar = 3;
+  // constructor(public http: Http) {
+  //   this.dolar = 3;
+  //   this.valorEquivalenteDollar = 0;
+  //   this.pontos = 0;
+  //   this.investimentoArredondado = 0;
+  //   this.totalPrimeiroAno = 0;
+  //   this.totalPrimeiroAnoRendimentos = 0;
+  //   this.totalPrimeiroAnoMes = 0;
+  //   this.getDollar();
+  // }
+
+  constructor() {
+    this.dolar = 3.30;
     this.valorEquivalenteDollar = 0;
     this.pontos = 0;
     this.investimentoArredondado = 0;
     this.totalPrimeiroAno = 0;
     this.totalPrimeiroAnoRendimentos = 0;
     this.totalPrimeiroAnoMes = 0;
-    this.getDollar();
   }
 
   //Busca e calcula o valor do dollar
-  getDollar() {
-    this.http.get('http://data.fixer.io/api/latest?access_key=5d0eb200bb9fb42872430c207728160f')
-      .map(res => res.json()).subscribe(data => {
-        let taxaDolar = data.rates['USD'];
-        let taxaReal = data.rates['BRL'];
-        let valorDolarReal = (taxaReal/taxaDolar).toFixed(2);
-        this.dolar = valorDolarReal;
-    });
-  }
+  // getDollar() {
+  //   this.http.get('http://data.fixer.io/api/latest?access_key=5d0eb200bb9fb42872430c207728160f')
+  //     .map(res => res.json()).subscribe(data => {
+  //       let taxaDolar = data.rates['USD'];
+  //       let taxaReal = data.rates['BRL'];
+  //       let valorDolarReal = (taxaReal/taxaDolar).toFixed(2);
+  //       this.dolar = valorDolarReal;
+  //   });
+  // }
 
   //método chamado pelo clique do botão simular
   getSimular() {
-    this.investimentoArredondado = (this.investimento - (this.investimento*0.01)).toFixed(2);
+    this.marred(this.investimento,165);
     this.valorEquivalenteDollar = (this.investimentoArredondado/this.dolar).toFixed(2);
-    this.pontos = (this.valorEquivalenteDollar / 50).toFixed(1);
+    this.pontos = ((this.valorEquivalenteDollar / 50)*8).toFixed(1);
     this.calculoMeses(this.investimentoArredondado, this.taxa);
+  }
+
+  marred(numero:any, multiplo:any) {
+    //50 dolares x 3,30 da 165 reais
+    let quantidade = numero / multiplo;
+    this.investimentoArredondado = Math.trunc(quantidade) * 165;
   }
 
   //Cálculo dos rendimentos do primeiro e segundo ano
@@ -58,10 +74,9 @@ export class Tab1Page {
       this.primeiroAno[i] = (investimento*(taxa/100));
       this.totalPrimeiroAno = this.totalPrimeiroAno + this.primeiroAno[i];
     }
-    console.log(this.totalPrimeiroAno);
-    console.log(investimento);
     this.totalPrimeiroAnoRendimentos = (parseFloat(investimento) + parseFloat(this.totalPrimeiroAno)).toFixed(2);
-
+    this.totalPrimeiroAnoMes = this.primeiroAno[0];
+    this.totalPrimeiroAno = this.totalPrimeiroAno.toFixed(2);
   }
 
 }
