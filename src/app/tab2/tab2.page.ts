fa52432ebcf4ad535e8bf94158c9894be8120319
Víctor Romário <victor.romario.seriado@gmail.com>
2019-07-04 18:26:58 -0300

    Primeira versão dos gráficos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component} from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
 import { ToastController } from '@ionic/angular';
+import { ViewChild, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-tab2',
@@ -9,12 +11,93 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  data: Observable<any[]>;
-  ref: AngularFireList<any>;
 
   constructor(private db: AngularFireDatabase, private toastCtrl: ToastController){
 
   }
+
+  @ViewChild('lineCanvas') lineCanvas;
+  lineChart: any;
+
+  ngOnInit() {
+    
+    this.lineChartMethod();
+  }
+
+  lineChartMethod() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement,
+      {
+         type: 'line',
+         data: {
+            labels: this.chartLabels,
+            datasets: [{
+               label                 : 'Daily Technology usage',
+               data                  : this.chartValues,
+               duration              : 2000,
+               easing                : 'easeInQuart',
+               backgroundColor       : this.chartColours,
+               hoverBackgroundColor  : this.chartHoverColours,
+               fill 				   : false
+            }]
+         },
+         options : {
+            maintainAspectRatio: false,
+            legend         : {
+               display     : true,
+               boxWidth    : 80,
+               fontSize    : 15,
+               padding     : 0
+            },
+            scales: {
+               yAxes: [{
+                  ticks: {
+                     beginAtZero:true,
+                     stepSize: 5,
+                     max : 100
+                  }
+               }],
+               xAxes: [{
+                  ticks: {
+                     autoSkip: false
+                  }
+               }]
+            }
+         }
+      });
+
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho'],
+        datasets: [
+            {
+                label: 'Últimos 7 meses',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(234, 176, 67, 0.4)',
+                borderColor: 'rgba(234, 176, 67, 1)',
+                borderCapStyle: 'square',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'round',
+                pointBorderColor: 'rgba(234, 176, 67, 1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(234, 176, 67, 1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+                spanGaps: false,
+            }
+        ]
+      }
+    });
+  }
+
+
 
   async updateToast() {
     let toast = await this.toastCtrl.create({
